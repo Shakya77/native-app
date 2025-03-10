0cfeddb8d7e7c47f27443084368f42060f1903a3
Shakya77 <bijanshakya145@gmail.com>
2025-03-10 19:25:45 +0545

    changes

---
+++ constants/images.ts
@@ -0,0 +1,9 @@
+import bg from "@/assets/images/bg.png";
+import highlights from "@/assets/images/highlight.png";
+import rankingGradient from "@/assets/images/rankingGradient.png";
+
+export const images = {
+  bg,
+  highlights,
+  rankingGradient,
+};
